15a5b670ef4923d29d4ab1cf52f8f63d78e5a602
ShomisMohammed <61913581+ShomisMohammed@users.noreply.github.com>
2020-03-14 10:18:30 +0100

    first commit

--- j.sh
+++ j.sh
@@ -1,2 +1,9 @@
-read link
-echo $link
+#!/bin/bash
+git init
+git status
+git add .
+git status
+git commit -m "first commit" .
+git status
+git remote add origin https://github.com/ShomisMohammed/shomis.git
+git push -u origin master

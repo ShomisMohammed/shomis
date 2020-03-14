46abf8645f507c2755f439898f67619886d4243c
ShomisMohammed <61913581+ShomisMohammed@users.noreply.github.com>
2020-03-14 10:28:38 +0100

    first commit

--- NewProjectsDownloader.sh
+++ NewProjectsDownloader.sh
@@ -5,5 +5,6 @@ git add .
 git status
 git commit -m "first commit" .
 git status
-git remote add origin github.com/ShomisMohammed/shomis.git
+git remote add origin https://github.com/ShomisMohammed/shomis.git
+git remote set-url origin https://github.com/ShomisMohammed/shomis.git
 git push -u origin master
